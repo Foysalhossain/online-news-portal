431691caf7b7a1da87be8d32b89627ab8d85c864
Foysal Hossain <foysalhossain772@gmail.com>
2023-12-20 12:45:42 +0600

    added marquee and latest button

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,15 +1,23 @@
+import { Button, Container } from 'react-bootstrap';
 import logo from '../../../assets/logo.png';
 import moment from 'moment';
+import Marquee from "react-fast-marquee";
 
 const Header = () => {
     return (
-        <div>
+        <Container>
             <div className="text-center">
                 <img src={logo} alt="" />
                 <p className='text-secondary'><small>Journalism Without Fear or Favour</small></p>
                 <p>{moment().format("dddd, MMMM D, YYYY")}</p>
             </div>
-        </div>
+            <div className='d-flex p-3 bg-secondary rounded'>
+                <Button variant="danger">Latest</Button>
+                <Marquee className='text-white m-2'>
+                    Match Highlights: Germany vs Spain — as it happened   !   Match Highlights: Germany vs Spain as...
+                </Marquee>
+            </div>
+        </Container>
     );
 };
 
