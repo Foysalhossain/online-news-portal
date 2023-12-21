acaae8a3254331ff9cf4cea1e253398d2ad70b1a
Foysal Hossain <foysalhossain772@gmail.com>
2023-12-21 17:57:36 +0600

    find us on section added

--- src/pages/Shared/RightNav/RightNav.jsx
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -1,15 +1,23 @@
-import { Button } from "react-bootstrap";
-import { FaGoogle, FaGithub } from "react-icons/fa";
-
+import { Button, ListGroup } from "react-bootstrap";
+import { FaGoogle, FaGithub, FaFacebook, FaInstagram } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 
 
 
 const RightNav = () => {
     return (
         <div>
-            <h4>Login With</h4>
+            <h4 className="mb-3">Login With</h4>
             <Button className="mb-2" variant="outline-primary"><FaGoogle /> Login with Google</Button>
             <Button variant="outline-secondary"><FaGithub /> Login with Github</Button>
+            <div className="my-4">
+                <h4 className="mb-3">Find us on</h4>
+                <ListGroup>
+                    <ListGroup.Item><FaFacebook /> Facebook</ListGroup.Item>
+                    <ListGroup.Item><FaXTwitter /> Twitter</ListGroup.Item>
+                    <ListGroup.Item><FaInstagram /> Instagram</ListGroup.Item>
+                </ListGroup>
+            </div>
         </div>
     );
 };
