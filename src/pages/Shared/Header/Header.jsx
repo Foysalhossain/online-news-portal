9b8ebd3db59784a4af60c0fe7f5a9fa899686765
Foysal Hossain <foysalhossain772@gmail.com>
2023-12-27 12:26:05 +0600

    firebase authentication and home page route create

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -2,8 +2,14 @@ import { Button, Container, Nav, Navbar } from 'react-bootstrap';
 import logo from '../../../assets/logo.png';
 import moment from 'moment';
 import Marquee from "react-fast-marquee";
+import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import { AuthContext } from '../../../providers/AuthProvider';
 
 const Header = () => {
+
+    const { user } = useContext(AuthContext);
+
     return (
         <Container className='py-4'>
             <div className="text-center">
@@ -22,12 +28,14 @@ const Header = () => {
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="mx-auto fw-bold">
-                            <Nav.Link href="#features">Home</Nav.Link>
+                            <Nav.Link href="#features">
+                                <Link to='/'>Home</Link>
+                            </Nav.Link>
                             <Nav.Link href="#pricing">About</Nav.Link>
                             <Nav.Link href="#pricing">Career</Nav.Link>
                         </Nav>
                         <Nav className='d-flex align-items-center'>
-                            <Nav.Link href="#deets">Profile</Nav.Link>
+                            <Nav.Link href="#deets">{user.displayName}</Nav.Link>
                             <Nav.Link eventKey={2} href="#memes">
                                 <Button variant="warning rounded-0">Login</Button>
                             </Nav.Link>
