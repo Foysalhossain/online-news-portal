9ee1a4d8b3198e3563c8d56a2d7fd3459defd1dc
Foysal Hossain <foysalhossain772@gmail.com>
2023-12-27 22:37:17 +0600

    some route changes

--- src/pages/Shared/NavigationBar/NavigationBar.jsx
+++ src/pages/Shared/NavigationBar/NavigationBar.jsx
@@ -4,9 +4,8 @@ import { FaUserCircle } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../../providers/AuthProvider";
 
-
 const NavigationBar = () => {
-    const {user} = useContext(AuthContext); 
+    const { user } = useContext(AuthContext);
     return (
         <Container>
             <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary mb-4">
@@ -14,22 +13,22 @@ const NavigationBar = () => {
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="mx-auto fw-bold">
-                            <Nav.Link href="#features">
-                                <Link to='/'>Home</Link>
-                            </Nav.Link>
-                            <Nav.Link href="#pricing">About</Nav.Link>
-                            <Nav.Link href="#pricing">Career</Nav.Link>
+                            <Link to='/' className="text-dark text-decoration-none" >Home</Link>
+                            <Link className="text-dark ps-2 text-decoration-none" >About</Link>
+                            <Link className="text-dark ps-2 text-decoration-none" >Carear</Link>
+                            {/* <Nav.Link href="#pricing">About</Nav.Link>
+                            <Nav.Link href="#pricing">Career</Nav.Link> */}
                         </Nav>
                         <Nav className='d-flex align-items-center'>
-                            {user &&
-                                <Nav.Link href="#deets"><FaUserCircle className='fs-2' /></Nav.Link>
+                            {
+                                user &&
+                                <FaUserCircle className='fs-2 me-2' />
                             }
-                            <Nav.Link eventKey={2} href="#memes">
-                                {user ?
+                            {
+                                user ?
                                     <Link><Button variant="warning rounded-0">Logout</Button></Link> :
-                                    <Link><Button variant="warning rounded-0">Login</Button></Link>
-                                }
-                            </Nav.Link>
+                                    <Link to='/login'><Button variant="warning rounded-0">Login</Button></Link>
+                            }
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
